eeb20f69e785ac58725cff2efe5c38e157a906a3
morito242 <63407059+morito242@users.noreply.github.com>
2022-01-10 21:44:09 +0700

    update back-end

--- back-end/app/common/connect.js
+++ back-end/app/common/connect.js
@@ -2,16 +2,22 @@
 
 var mysql = require('mysql');
 
+// var connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'group7_sql'
+// });
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'group7_sql'
+    host: 'buqqnotrtb0rzfscllux-mysql.services.clever-cloud.com',
+    user: 'ubvbxneuwditohox',
+    password: 'vaVzlqgsEWeOEYYQwZ1Q',
+    database: 'buqqnotrtb0rzfscllux'
 });
-
 
 connection.connect(function(err) {
     if(err){
+        console.log(err);
         console.log("Connecttion database failed !!!");
     }
     else{
