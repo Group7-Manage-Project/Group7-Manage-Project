960200a1ee1f9d923dbe7b8118c4b205389e1af0
tran.hoang <hoang.nt@cyberlogitec.com>
2022-01-16 00:20:31 +0700

    update front-end

--- group7_sql.sql
+++ group7_sql.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 11, 2022 at 06:23 PM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 7.4.21
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th1 15, 2022 lúc 06:18 PM
+-- Phiên bản máy phục vụ: 10.4.20-MariaDB
+-- Phiên bản PHP: 7.4.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `group7_sql`
+-- Cơ sở dữ liệu: `group7_sql`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `category_task`
+-- Cấu trúc bảng cho bảng `category_task`
 --
 
 CREATE TABLE `category_task` (
@@ -39,7 +39,7 @@ CREATE TABLE `category_task` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `category_task`
+-- Đang đổ dữ liệu cho bảng `category_task`
 --
 
 INSERT INTO `category_task` (`CATEGORY_TASK_ID`, `CATEGORY_NAME`, `CREATE_DATE`, `CREATE_USER`, `UPDATE_DATE`, `UPDATE_USER`, `DEPARTMENT_ID`, `DELETE_FLG`) VALUES
@@ -47,12 +47,17 @@ INSERT INTO `category_task` (`CATEGORY_TASK_ID`, `CATEGORY_NAME`, `CREATE_DATE`,
 (5, 'find', '2021-01-01', 'John', '2021-01-02', 'Marco', 2, 'flag'),
 (6, 'read', NULL, 'Math', NULL, 'Witch', 1, NULL),
 (11, 'Digital Marketing', '2022-01-07', 'John Switch', '2022-01-07', 'John Switch', 13, 'N'),
-(12, 'Administration', '2022-01-10', 'Nguyễn Trần Hoàng', '2022-01-10', 'Nguyễn Trần Hoàng', 18, 'N');
+(12, 'Administration', '2022-01-10', 'Nguyễn Trần Hoàng', '2022-01-10', 'Nguyễn Trần Hoàng', 18, 'N'),
+(14, 'Public Financial Management.', '2022-01-12', 'John Switch', '2022-01-12', 'John Switch', 19, 'N'),
+(15, 'Public Relations', '2022-01-16', 'John Switch', '2022-01-16', 'John Switch', 24, 'N'),
+(16, 'Business Administration', '2022-01-16', 'John Switch', '2022-01-16', 'John Switch', 20, 'N'),
+(17, 'International bussiness', '2022-01-16', 'John Switch', '2022-01-16', 'John Switch', 21, 'N'),
+(18, 'Commercial business', '2022-01-16', 'John Switch', '2022-01-16', 'John Switch', 22, 'N');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `comment_task`
+-- Cấu trúc bảng cho bảng `comment_task`
 --
 
 CREATE TABLE `comment_task` (
@@ -66,7 +71,7 @@ CREATE TABLE `comment_task` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `comment_task`
+-- Đang đổ dữ liệu cho bảng `comment_task`
 --
 
 INSERT INTO `comment_task` (`COMMENT_TASK_ID`, `USER_COMMENT_ID`, `PHASE_NAME`, `COMMENT`, `CREATE_COMMENT_DATE`, `TASK_ID`, `FILE`) VALUES
@@ -133,7 +138,6 @@ INSERT INTO `comment_task` (`COMMENT_TASK_ID`, `USER_COMMENT_ID`, `PHASE_NAME`, 
 (71, 13, 'Comment', 'tttt', '2022-01-11 00:00:00', 27, ''),
 (72, 13, 'Register', 'abcd', '2022-01-11 00:00:00', 27, ''),
 (73, 13, 'Register', 'fkjsfkds', '2022-01-11 00:00:00', 27, ''),
-(74, 13, 'Comment', 'as', '2022-01-12 00:00:00', 28, ''),
 (75, 13, 'Comment', 'ssss', '2022-01-12 00:00:00', 1, ''),
 (76, 13, 'Comment', 'elwin', '2022-01-12 00:00:00', 1, ''),
 (77, 13, 'Register', 'ddddd', '2022-01-12 00:00:00', 1, ''),
@@ -145,12 +149,28 @@ INSERT INTO `comment_task` (`COMMENT_TASK_ID`, `USER_COMMENT_ID`, `PHASE_NAME`, 
 (83, 13, 'Comment', 'aaaa', '2022-01-12 00:00:00', 30, ''),
 (84, 13, 'Comment', 'elwin2', '2022-01-12 00:00:00', 1, ''),
 (85, 13, 'Comment', 'elwin study\n', '2022-01-12 00:00:00', 1, ''),
-(86, 13, 'Comment', 'qưerty', '2022-01-12 00:00:00', 30, '');
+(86, 13, 'Comment', 'qưerty', '2022-01-12 00:00:00', 30, ''),
+(88, 1, 'Comment', 'dear sirr', '2022-01-12 00:00:00', 30, ''),
+(89, 1, 'Comment', 'dear sir', '2022-01-12 00:00:00', 30, ''),
+(91, 1, 'Comment', '28', '2022-01-12 00:00:00', 30, ''),
+(92, 1, 'Comment', '30', '2022-01-12 00:00:00', 30, ''),
+(93, 1, 'Comment', '28', '2022-01-12 00:00:00', 30, ''),
+(94, 1, 'Comment', '30', '2022-01-12 00:00:00', 30, ''),
+(96, 1, 'Comment', '30', '2022-01-12 00:00:00', 30, ''),
+(99, 1, 'Comment', '24', '2022-01-12 00:00:00', 30, ''),
+(100, 1, 'Confirmation', 'aaaa', '2022-01-12 00:00:00', 30, '1641961818219--documentation.docx'),
+(101, 1, 'Confirmation', 'dear sir', '2022-01-12 00:00:00', 30, '1641962025456--documentation.docx'),
+(102, 1, 'Confirmation', 'dear sirr', '2022-01-12 00:00:00', 29, ''),
+(103, 1, 'Comment', 'Dear Mr Vũ Nhật Minh Trường. Please confirm help me.', '2022-01-12 00:00:00', 28, ''),
+(104, 1, 'Register', 'Dear Trường: Keep going', '2022-01-12 00:00:00', 28, ''),
+(105, 1, 'Confirmation', 'Dear sir, i update guide line', '2022-01-12 00:00:00', 28, '1641978749332--documentation.docx'),
+(106, 1, 'Comment', 'Hello sirr', '2022-01-16 00:00:00', 4, ''),
+(107, 1, 'Register', 'I finished the task. Pleased check to help me again', '2022-01-16 00:00:00', 4, '');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `departments`
+-- Cấu trúc bảng cho bảng `departments`
 --
 
 CREATE TABLE `departments` (
@@ -164,7 +184,7 @@ CREATE TABLE `departments` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `departments`
+-- Đang đổ dữ liệu cho bảng `departments`
 --
 
 INSERT INTO `departments` (`DEPARTMENT_ID`, `DEPARTMENT_NAME`, `DESCRIPTION`, `CREATE_DATE`, `CREATE_USER`, `UPDATE_DATE`, `UPDATE_USER`) VALUES
@@ -173,12 +193,19 @@ INSERT INTO `departments` (`DEPARTMENT_ID`, `DEPARTMENT_NAME`, `DESCRIPTION`, `C
 (3, 'Department management', NULL, NULL, NULL, NULL, NULL),
 (13, 'Marketing', 'Marketing', '2022-01-07', 'John Switch', '2022-01-07', 'John Switch'),
 (17, '13noi chuoiMarketing', '', '2022-01-07', 'John Switch', '2022-01-07', 'John Switch'),
-(18, 'Faculty of International Business Administration', 'International Business Administration', '2022-01-10', 'Nguyễn Trần Hoàng', '2022-01-10', 'Nguyễn Trần Hoàng');
+(18, 'Faculty of International Business Administration', 'International Business Administration', '2022-01-10', 'Nguyễn Trần Hoàng', '2022-01-10', 'Nguyễn Trần Hoàng'),
+(19, 'Finance', 'Finance is a broad term that describes activities associated with banking, leverage or debt, credit, capital markets, money, and investments. Basically, finance represents money management and the process of acquiring needed funds. Finance also encompasses the oversight, creation, and study of money, banking, credit, investments, assets, and liabilities that make up financial systems.', '2022-01-12', 'John Switch', '2022-01-12', 'John Switch'),
+(20, 'Business Administration', 'Business Administration', '2022-01-16', 'John Switch', '2022-01-16', 'John Switch'),
+(21, 'International bussiness', 'International bussiness', '2022-01-16', 'John Switch', '2022-01-16', 'John Switch'),
+(22, 'Commercial business', 'Commercial business', '2022-01-16', 'John Switch', '2022-01-16', 'John Switch'),
+(23, 'Logistics and supply chain management', 'Logistics and supply chain management', '2022-01-16', 'John Switch', '2022-01-16', 'John Switch'),
+(24, 'Public Relations', 'Public Relations', '2022-01-16', 'John Switch', '2022-01-16', 'John Switch'),
+(25, 'Public Relations', 'Public Relations', '2022-01-16', 'John Switch', '2022-01-16', 'John Switch');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `schedule`
+-- Cấu trúc bảng cho bảng `schedule`
 --
 
 CREATE TABLE `schedule` (
@@ -196,7 +223,7 @@ CREATE TABLE `schedule` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `schedule`
+-- Đang đổ dữ liệu cho bảng `schedule`
 --
 
 INSERT INTO `schedule` (`Id`, `IsAllDay`, `StartTime`, `EndTime`, `Subject`, `Description`, `Location`, `RecurrenceRule`, `StartTimezone`, `EndTimezone`, `EMPLOYEE_ID`) VALUES
@@ -208,12 +235,14 @@ INSERT INTO `schedule` (`Id`, `IsAllDay`, `StartTime`, `EndTime`, `Subject`, `De
 (9, 1, '2022-01-22 17:00:00', '2022-01-23 17:00:00', 'finished update schedule', NULL, NULL, NULL, NULL, NULL, 1),
 (11, 1, '2022-01-13 17:00:00', '2022-01-14 17:00:00', 'Submit a report', NULL, NULL, NULL, NULL, NULL, 1),
 (12, 1, '2022-01-11 17:00:00', '2022-01-13 17:00:00', 'abcdef', NULL, 'ácd', NULL, NULL, NULL, 14),
-(13, 1, '2022-01-11 17:00:00', '2022-01-12 17:00:00', 'ád', NULL, NULL, NULL, NULL, NULL, 14);
+(13, 1, '2022-01-11 17:00:00', '2022-01-12 17:00:00', 'ád', NULL, NULL, NULL, NULL, NULL, 14),
+(14, 1, '2022-02-14 17:00:00', '2022-02-15 17:00:00', 'test', 'Test code agian', 'home', NULL, NULL, NULL, 1),
+(15, 1, '2022-01-26 17:00:00', '2022-01-27 17:00:00', 'test code', NULL, 'home', NULL, NULL, NULL, 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `staff`
+-- Cấu trúc bảng cho bảng `staff`
 --
 
 CREATE TABLE `staff` (
@@ -230,24 +259,25 @@ CREATE TABLE `staff` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `staff`
+-- Đang đổ dữ liệu cho bảng `staff`
 --
 
 INSERT INTO `staff` (`EMPLOYEE_ID`, `FULL_NAME`, `USER_NAME`, `PASSWORD`, `BIRTH_DAY`, `EMAIL`, `IMAGE`, `POSITION`, `ROLL`, `DELETE_FLAG`) VALUES
 (1, 'John Switch', 'John', '1234', '2000-11-11', 'abc@gmail.com', '1641177573530--logo.jpg', 'Leader', 'Admin', NULL),
-(2, 'John Math', 'Math', '4567', '1989-12-19', 'def@gmail.com', NULL, 'xxxxx', 'xxxxx', NULL),
+(2, 'John Math', 'Math', '4567', '2000-11-11', 'def@gmail.com', '1642266767933--background.jpg', 'Leader', 'Admin', NULL),
 (7, 'Vũ Nhật Minh Trường', 'truong2', '1234', '2000-04-11', 'truongvnmt18@uef.edu.vn', '1640599576538--Phut.jpg', 'Leader', 'User', NULL),
 (8, 'Vũ Nhật Minh Trường', 'truong3', '1234', '2000-01-11', 'truongvnmt18@uef.edu.vn', '1640599915070--Phut.jpg', 'Leader', 'Admin', NULL),
 (9, 'Vũ Nhật Minh Trường', 'truong4', '1234', '2000-04-11', 'abcdef@dounets.com', '1640600070519--Phut.jpg', 'Leader', 'Admin', NULL),
 (10, 'Nguyễn Trần Hoàng', 'hoang10', '1234', '2000-04-11', 'abc@dounets.com', '1640601729704--hoang2.JPG', 'Leader', 'Admin', NULL),
-(11, 'Nguyễn Trần Hoàng', 'hoang', '1234', '2000-11-04', 'hoangnt18@uef.edu.vn', NULL, 'Leader', 'Admin', NULL),
+(11, 'Nguyễn Trần Hoàng', 'hoang', '1234', '2000-11-04', 'hoangnt18@uef.edu.vn', '1642266861196--hoangFB.jpg', 'Leader', 'Admin', NULL),
 (13, 'Nguyễn Trần Hoàng', 'admin', '1234', '2000-04-11', 'abc@dounets.com', '1640757469008--Phut.jpg', 'Leader', 'Admin', NULL),
-(14, 'Nguyễn Thị Thu Quyên', 'admin1', '123456', '2022-01-04', 'abcd@gmaiil.com', '1641868000314--web.ico', 'Developer', 'User', NULL);
+(14, 'Nguyễn Thị Thu Quyên', 'quyenntt', '123456', '2022-01-04', 'quyenntt18@uef.edu.vn', '1642266838579--background.jpg', 'Leader', 'Admin', NULL),
+(15, 'Admin1', 'admin1', '1234', '2000-11-11', 'abc@dounets.vn', '1641979460275--croped_1614587506079-25,7,142,123-1.5-.png', 'Leader', 'Admin', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `task`
+-- Cấu trúc bảng cho bảng `task`
 --
 
 CREATE TABLE `task` (
@@ -276,13 +306,13 @@ CREATE TABLE `task` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `task`
+-- Đang đổ dữ liệu cho bảng `task`
 --
 
 INSERT INTO `task` (`TASK_ID`, `JOB`, `STATUS`, `CATEGORY`, `TITLE`, `PROGRESS`, `EFFORT`, `IMPORTANT`, `DESCRIPTION`, `FILE`, `ASSIGNEE_ID`, `REGISTER_USER_ID`, `CONFIRMATION_ID`, `IMPLEMENTATION_ID`, `TEST_ID`, `APPROVAL_ID`, `FINISH_ID`, `START_DATE`, `END_DATE`, `STEP`, `CATEGORY_TASK_ID`, `DEPARTMENT_NAME`) VALUES
 (1, 'Develope', 'IN-PROGRESS', 'DEK', 'Develop System', 25, 60, 'Very Important', 'Develop System and result', NULL, 2, 1, 2, 1, 2, 1, 2, '2021-12-19', '2021-12-25', 2, 4, NULL),
 (2, 'Bug Fixing', 'IN-PROGRESS', 'KFC', 'Fix bug UI', 5, 100, 'Important', 'Fix bug UI', NULL, 2, 2, 1, 2, 1, 2, 1, '2021-11-29', '2021-12-25', 3, 5, NULL),
-(4, 'Documentation', 'IN-PROGRESS', 'KFC', 'Document of website', 10, 30, 'Important', 'Document of the website to use', '1638438866699--1638437628123--bai-tap-tuan-1.docx', 1, 1, 1, 1, 1, 1, 1, '2021-12-02', '2021-12-25', 1, 4, NULL),
+(4, 'Documentation', 'IN-PROGRESS', 'KFC', 'Document of website', 25, 60, 'Important', 'Document of the website to use', '1638438866699--1638437628123--bai-tap-tuan-1.docx', 1, 1, 1, 1, 1, 1, 1, '2021-12-02', '2021-12-25', 2, 4, NULL),
 (5, 'Bug Fixing', 'IN-PROGRESS', 'code', 'Fix Bug UI', 10, 30, 'Very Important', 'Fix Bug UI', '1640865635072--review .docx', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2021-12-30', '0000-00-00', 1, NULL, '1noi chuoiFaculty of Information Technology'),
 (6, 'Bug Fixing', 'IN-PROGRESS', '', 'Fix bug UI', 10, 30, 'Very Important', 'Fix bug UI', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2021-12-30', '0000-00-00', 1, NULL, ''),
 (7, 'Bug Fixing', 'IN-PROGRESS', '', 'Fix bug UI', 10, 30, 'Very Important', 'Fix bug UI', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2021-12-30', '0000-00-00', 1, NULL, ''),
@@ -301,23 +331,25 @@ INSERT INTO `task` (`TASK_ID`, `JOB`, `STATUS`, `CATEGORY`, `TITLE`, `PROGRESS`,
 (25, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2021-12-31', NULL, NULL, NULL, NULL),
 (26, 'Documentation', 'IN-PROGRESS', 'find', 'Find document', 55, 120, 'Normal', 'Find document for project', '1641290603163--review .docx', 7, 7, 8, 10, 7, 9, 13, '2022-01-04', '2022-01-11', 4, 5, 'Department of E-Commerce'),
 (27, 'Training', 'IN-PROGRESS', 'find', 'Training SQL', 25, 60, 'Very Low', 'Training SQL', '1641830919054--list-task-file-excel.xlsx', 9, 7, 9, 8, 10, 10, 10, '2022-01-04', '2022-01-11', 2, 5, 'Department of E-Commerce'),
-(28, 'Develope', 'IN-PROGRESS', 'Digital Marketing', 'Sale product', 10, 30, 'Very Important', 'Meet clinets', '', 10, 10, 9, 9, 9, 8, 10, '2022-01-07', '2022-01-14', 1, 11, 'Marketing'),
-(29, 'Research', 'IN-PROGRESS', 'code', 'Search in website', 25, 60, 'Very Important', 'Code search', '', 8, 7, 8, 9, 10, 11, 13, '2022-01-10', '2022-01-20', 2, 4, 'Faculty of Information Technology'),
-(30, 'Bug Fixing', 'IN-PROGRESS', 'Administration', 'Administration', 25, 60, 'Very Important', 'Administration vs A..', '', 2, 1, 2, 7, 10, 13, 8, '2022-01-10', '2022-01-29', 2, 12, 'Faculty of International Business Administration');
+(28, 'Develope', 'IN-PROGRESS', 'Digital Marketing', 'Sale product', 40, 90, 'Very Important', 'Meet clinets', '1641978749341--documentation.docx', 1, 10, 9, 9, 9, 8, 10, '2022-01-07', '2022-01-14', 3, 11, 'Marketing'),
+(29, 'Research', 'IN-PROGRESS', 'code', 'Search in website', 40, 90, 'Very Important', 'Code search', '', 7, 7, 8, 9, 10, 11, 13, '2022-01-10', '2022-01-20', 3, 4, 'Faculty of Information Technology'),
+(30, 'Bug Fixing', 'IN-PROGRESS', 'Administration', 'Administration', 40, 90, 'Very Important', 'Administration vs A..', '1641962025463--documentation.docx', 7, 1, 2, 7, 10, 13, 8, '2022-01-10', '2022-01-29', 3, 12, 'Faculty of International Business Administration'),
+(31, 'Bug Fixing', 'In Processing', 'Public Financial Management.', 'Fix Ui sceen Task', 10, 30, 'Very Important', 'Fix Ui sceen Task. My boss need fix it', '1641983011549--documentation.docx', 15, 15, 13, 8, 7, 10, 15, '2022-01-12', '2022-01-19', 1, 14, 'Finance'),
+(32, 'Documentation', 'In Processing', 'Public Relations', 'Write document', 10, 30, 'Normal', 'Write document', '1642267052555--documentation.docx', 1, 1, 7, 11, 14, 15, 8, '2022-01-16', '2022-01-23', 1, 15, 'Public Relations');
 
 --
--- Indexes for dumped tables
+-- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Indexes for table `category_task`
+-- Chỉ mục cho bảng `category_task`
 --
 ALTER TABLE `category_task`
   ADD PRIMARY KEY (`CATEGORY_TASK_ID`),
   ADD KEY `DEPARTMENT_ID` (`DEPARTMENT_ID`);
 
 --
--- Indexes for table `comment_task`
+-- Chỉ mục cho bảng `comment_task`
 --
 ALTER TABLE `comment_task`
   ADD PRIMARY KEY (`COMMENT_TASK_ID`),
@@ -325,26 +357,26 @@ ALTER TABLE `comment_task`
   ADD KEY `USER_COMMENT_ID` (`USER_COMMENT_ID`);
 
 --
--- Indexes for table `departments`
+-- Chỉ mục cho bảng `departments`
 --
 ALTER TABLE `departments`
   ADD PRIMARY KEY (`DEPARTMENT_ID`);
 
 --
--- Indexes for table `schedule`
+-- Chỉ mục cho bảng `schedule`
 --
 ALTER TABLE `schedule`
   ADD PRIMARY KEY (`Id`),
   ADD KEY `EMPLOYEE_ID` (`EMPLOYEE_ID`);
 
 --
--- Indexes for table `staff`
+-- Chỉ mục cho bảng `staff`
 --
 ALTER TABLE `staff`
   ADD PRIMARY KEY (`EMPLOYEE_ID`);
 
 --
--- Indexes for table `task`
+-- Chỉ mục cho bảng `task`
 --
 ALTER TABLE `task`
   ADD PRIMARY KEY (`TASK_ID`),
@@ -358,70 +390,70 @@ ALTER TABLE `task`
   ADD KEY `FINISH_ID` (`FINISH_ID`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT cho các bảng đã đổ
 --
 
 --
--- AUTO_INCREMENT for table `category_task`
+-- AUTO_INCREMENT cho bảng `category_task`
 --
 ALTER TABLE `category_task`
-  MODIFY `CATEGORY_TASK_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `CATEGORY_TASK_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
--- AUTO_INCREMENT for table `comment_task`
+-- AUTO_INCREMENT cho bảng `comment_task`
 --
 ALTER TABLE `comment_task`
-  MODIFY `COMMENT_TASK_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=87;
+  MODIFY `COMMENT_TASK_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=108;
 
 --
--- AUTO_INCREMENT for table `departments`
+-- AUTO_INCREMENT cho bảng `departments`
 --
 ALTER TABLE `departments`
-  MODIFY `DEPARTMENT_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `DEPARTMENT_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
--- AUTO_INCREMENT for table `schedule`
+-- AUTO_INCREMENT cho bảng `schedule`
 --
 ALTER TABLE `schedule`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
--- AUTO_INCREMENT for table `staff`
+-- AUTO_INCREMENT cho bảng `staff`
 --
 ALTER TABLE `staff`
-  MODIFY `EMPLOYEE_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `EMPLOYEE_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
--- AUTO_INCREMENT for table `task`
+-- AUTO_INCREMENT cho bảng `task`
 --
 ALTER TABLE `task`
-  MODIFY `TASK_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `TASK_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
--- Constraints for dumped tables
+-- Các ràng buộc cho các bảng đã đổ
 --
 
 --
--- Constraints for table `category_task`
+-- Các ràng buộc cho bảng `category_task`
 --
 ALTER TABLE `category_task`
   ADD CONSTRAINT `category_task_ibfk_1` FOREIGN KEY (`DEPARTMENT_ID`) REFERENCES `departments` (`DEPARTMENT_ID`);
 
 --
--- Constraints for table `comment_task`
+-- Các ràng buộc cho bảng `comment_task`
 --
 ALTER TABLE `comment_task`
   ADD CONSTRAINT `comment_task_ibfk_1` FOREIGN KEY (`TASK_ID`) REFERENCES `task` (`TASK_ID`),
   ADD CONSTRAINT `comment_task_ibfk_2` FOREIGN KEY (`USER_COMMENT_ID`) REFERENCES `staff` (`EMPLOYEE_ID`);
 
 --
--- Constraints for table `schedule`
+-- Các ràng buộc cho bảng `schedule`
 --
 ALTER TABLE `schedule`
   ADD CONSTRAINT `schedule_ibfk_1` FOREIGN KEY (`EMPLOYEE_ID`) REFERENCES `staff` (`EMPLOYEE_ID`);
 
 --
--- Constraints for table `task`
+-- Các ràng buộc cho bảng `task`
 --
 ALTER TABLE `task`
   ADD CONSTRAINT `task_ibfk_1` FOREIGN KEY (`CATEGORY_TASK_ID`) REFERENCES `category_task` (`CATEGORY_TASK_ID`),
